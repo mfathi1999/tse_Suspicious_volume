1c637b0f89634fcea59c180ccc0b24705a8e9324
mohammad <fathim2000astronomy@gmail.com>
2021-10-29 17:27:35 +0330

    main class functions for download data and calcute suspicious volumn added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import pytse_client as tse
-import csv
-import pymongo
+import average
+
+def download_symbols_data():
+    tickers = tse.download(symbols="all", write_to_csv=True)
+
+def calcute_Suspicious_volume():
+    average.main()
+
+
+if __name__ == "__main__":
+    download_symbols_data()
+    calcute_Suspicious_volume()
